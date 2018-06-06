c6c79e3af49e8c755c562d0e0e4dacc16c205883
webistomin <webistomin@gmail.com>
2018-06-06 15:05:48 +0300

    Добавил скрипт для меню

---
+++ src/js/main-nav.js
@@ -0,0 +1,14 @@
+var navToggle = document.querySelector('.page-header__toggler');
+var navMain = document.querySelector('.main-nav');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+    navToggle.classList.add('page-header__toggler--opened')
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+    navToggle.classList.remove('page-header__toggler--opened');
+  }
+});
